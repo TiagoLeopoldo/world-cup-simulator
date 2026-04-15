72aba35c4a9dfd32b5c23752ebe062c72b2a377b
TiagoLeopoldo <tnleopoldo.dev@gmail.com>
2026-04-15 15:57:14 -0300

    feat: renderizar resultado simulado das partidas.

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     fetchTeams();
   }, []);
 
-  console.log(groups)
+  console.log(groups);
   return (
     <div>
       <h1>Grupos da Copa</h1>
@@ -34,7 +34,8 @@ function App() {
           <ul>
             {group.matches.map((match, index) => (
               <li key={index}>
-                {match.home.nome} vs {match.away.nome}
+                {match.home.nome} {match.homeGoals} x {match.awayGoals}{" "}
+                {match.away.nome}
               </li>
             ))}
           </ul>
