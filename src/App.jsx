a9318d916636d8cb7bbf03122301450b4e169f24
TiagoLeopoldo <tnleopoldo.dev@gmail.com>
2026-04-15 00:53:54 -0300

    feat: gerar na tela grupos de times sorteados

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { getTeams } from "./services/api";
+import { createGroups } from "./utils/groupDraw";
 
-const App = () => {
-  const [teams, setTeams] = useState([]);
+function App() {
+  const [groups, setGroups] = useState([]);
 
   useEffect(() => {
-    const fetchTeams = async () => {
-      try {
-        const data = await getTeams();
-        setTeams(data);
-      } catch (error) {
-        console.error("Error fetching teams:", error);
-      }
-    };
+    async function fetchTeams() {
+      const data = await getTeams();
+      const generatedGroups = createGroups(data);
+      setGroups(generatedGroups);
+    }
+
     fetchTeams();
   }, []);
-  console.log(teams);
+
   return (
     <div>
-      <h1>World Cup Simulator</h1>
-      <ul>
-        {teams.map((team) => (
-          <li key={team.token}>{team.nome}</li>
-        ))}
-      </ul>
+      <h1>Grupos da Copa</h1>
+
+      {groups.map((group) => (
+        <div key={group.group}>
+          <h2>Grupo {group.group}</h2>
+
+          <ul>
+            {group.teams.map((team) => (
+              <li key={team.token}>{team.nome}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
