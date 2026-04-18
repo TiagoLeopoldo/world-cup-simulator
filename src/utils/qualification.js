1c067bdf0420d98563df3bbca0beb7f8e1a7a735
TiagoLeopoldo <tnleopoldo.dev@gmail.com>
2026-04-17 22:46:16 -0300

    feat: implementar extração e renderização dos classificados da fase de grupos

---
+++ src/utils/qualification.js
@@ -0,0 +1,10 @@
+export const getQualifiedTeams = (groups) => {
+  const qualified = [];
+
+  groups.forEach((group) => {
+    const topTwo = group.standings.slice(0, 2);
+    qualified.push(...topTwo);
+  });
+
+  return qualified;
+};
